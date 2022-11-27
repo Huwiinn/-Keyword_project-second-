e7d157dc60d1201b809f17c2569560f66735257b
hwuiinn <gnldls8406@gmail.com>
2022-11-28 04:26:29 +0900

    1128 다경ver 덮어쓰기
    ㅈㄱㄴ

--- js/profile_update.js
+++
@@ -1,9 +0,0 @@
-export const pu_openModal = () => {
-  const modal = document.querySelector(".pu_modal");
-  modal.classList.remove("pu_hidden");
-};
-
-export const pu_closeModal = () => {
-  const modal = document.querySelector(".pu_modal");
-  modal.classList.add("pu_hidden");
-};
